e97b3dd76b29a59cac9ee9c47a31302c9ea98b62
Nikkrana28 <60419615+Nikkrana28@users.noreply.github.com>
2021-10-03 12:57:44 +0530

    Update Footer.tsx

--- packages/pancake-uikit/src/components/Footer/Footer.tsx
+++ packages/pancake-uikit/src/components/Footer/Footer.tsx
@@ -18,6 +18,7 @@ import { LogoWithTextIcon, ArrowForwardIcon } from "../Svg";
 import { Button } from "../Button";
 import { Colors } from "../..";
 
+
 const MenuItem: React.FC<FooterProps> = ({
   items,
   isDark,
@@ -86,7 +87,7 @@ const MenuItem: React.FC<FooterProps> = ({
             </Box>
             <Button
               as="a"
-              href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+              href="https://pancakeswap.finance/"
               target="_blank"
               scale="sm"
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
